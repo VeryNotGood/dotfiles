c4e16087b1c49878d60422f1d3bebe1063433620
sbel <shawnjbelmore@gmail.com>
2024-03-19 23:52:12 -0500

    less alpha is not more beta

--- nvim/lua/custom/chadrc.lua
+++ nvim/lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "tokyodark",
-  theme_toggle = { "tokyodark", "github_light" },
+  theme = "tokyonight",
+  theme_toggle = { "tokyonight", "github_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
